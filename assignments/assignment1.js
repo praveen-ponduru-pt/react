e4c3b040788e5a5f5261ee0ec4676d0dc021af43
praveen <praveen.ponduru@pal.tech>
2025-03-18 15:33:49 +0530

    assignment 1 and session 1

---
+++ assignments/assignment1.js
@@ -0,0 +1,13 @@
+let totalSeconds = 10
+function counter() {
+    console.log('Your OTP will expire in ' + totalSeconds + ' seconds');
+    totalSeconds--;
+}
+
+let timerId = setInterval(counter, 1000)
+
+setTimeout(() => {
+    clearInterval(timerId);
+    console.log('Your OTP is expired');
+}, 11000)
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Hello World')
+
+let a = [1, 2, 3]
+console.log(a)
+// map find findindex filter foreach 
