786e0520465b992ac975436e56a28d0724194c2d
praveen <praveen.ponduru@pal.tech>
2025-03-19 17:56:33 +0530

    udpated assignment 1

--- assignments/assignment1.js
+++ assignments/assignment1.js
@@ -1,4 +1,5 @@
 let totalSeconds = 10
+
 function counter() {
     console.log('Your OTP will expire in ' + totalSeconds + ' seconds');
     totalSeconds--;
@@ -9,5 +10,4 @@ let timerId = setInterval(counter, 1000)
 setTimeout(() => {
     clearInterval(timerId);
     console.log('Your OTP is expired');
-}, 11000)
-
+}, 10100)
